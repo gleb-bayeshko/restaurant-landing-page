dba79f397a7a3e5ceb3e6cc7266f4f1fda958257
GLEB BAYESHKO <bayeshko_gleb@mail.ru>
2020-09-26 19:22:36 +0300

    feat: implement carousel with bootstrap, some minor fixes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@ const burgerButtonClassActive = 'burger-menu_active';
 const navListClassActive = 'navbar__list_active';
 const overlayClassActive = 'overlay_active';
 const navLinks = document.querySelectorAll('.navbar__link');
+const carouselSlidesContainer = document.querySelector('.carousel-slides');
+const carouselSlides = document.querySelectorAll('.carousel-slide');
+const carouselControls = document.querySelectorAll('.carousel-controls-panel__control');
 
 const burgerMenu = new BurgerMenu(burgerButton, burgerButtonClassActive, navList, navListClassActive, overlay, overlayClassActive);
 burgerMenu.addBurgerButtonListener();
 
 const navigation = new Navigation(navLinks);
 navigation.addScrollOnNavClick();
+
+$('.carousel').carousel({
+    interval: 2000,
+    ride: 'carousel'
+  })
